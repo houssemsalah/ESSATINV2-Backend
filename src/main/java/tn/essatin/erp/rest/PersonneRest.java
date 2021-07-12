8ab868ea3438c26188f1941e2ee7bc5bd81831da
mednasr87@gmail.com <mednasr87@gmail.com>
2021-07-12 16:12:21 +0100

    add api  gettypeidentificateurs getlistnationalites getlistdiplomes for Inscription

--- src/main/java/tn/essatin/erp/rest/PersonneRest.java
+++ src/main/java/tn/essatin/erp/rest/PersonneRest.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tn.essatin.erp.dao.DiplomeDao;
+import tn.essatin.erp.dao.NationaliteDao;
 import tn.essatin.erp.dao.PersonneDao;
+import tn.essatin.erp.dao.TypeIdentificateurDao;
 
 import java.util.List;
 
@@ -17,9 +20,29 @@ import java.util.List;
 public class PersonneRest {
     @Autowired
     PersonneDao personneDao;
+    @Autowired
+    TypeIdentificateurDao typeIdentificateurDao;
+    @Autowired
+    NationaliteDao nationaliteDao;
+    @Autowired
+    DiplomeDao diplomeDao;
 
     @GetMapping("/getall")
     public ResponseEntity<?> getAll() {
         return new ResponseEntity<List>(personneDao.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/gettypeidentificateurs")
+    public ResponseEntity<?> getTypeIdentificateurs(){
+        return new ResponseEntity<List>(typeIdentificateurDao.findAll(),HttpStatus.OK);
+    }
+    @GetMapping("/getlistnationalites")
+    public ResponseEntity<?> getListNationalites(){
+        return new ResponseEntity<List>(nationaliteDao.findAll(),HttpStatus.OK);
+    }
+    @GetMapping("/getlistdiplomes")
+    public ResponseEntity<?> getListDiplomes(){
+        return new ResponseEntity<List>(diplomeDao.findAll(),HttpStatus.OK);
+    }
+
 }
