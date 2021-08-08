b74e73f96d2b5a5661e6fc6478873a878ec908e0
Jihen Eladel <jiheneladel7@gmail.com>
2021-08-08 22:49:59 +0100

    Modification financier by Med

--- src/main/java/tn/essatin/erp/dao/financier/ModeleContratDao.java
+++
@@ -1,8 +0,0 @@
-package tn.essatin.erp.dao.financier;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModeleContratDao extends JpaRepository<ModeleContrat, Integer> {
-}
